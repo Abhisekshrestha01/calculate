f57f4a33bbc86cf6fb84d08a4fd57058ef440890
anuragmadhav naalla <anurag.012@hotmail.com>
2018-09-23 22:41:02 -0500

    addsubdiv added

---
+++ Addsubdiv/addsubdiv.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addsubdiv
+{
+    public class addsubdiv
+    {
+        public float Divres(float Subres, float a)
+        {
+            return (Subres / a);
+        }
+    }
+}
