c86a69b8e4f20bc6035df069115debb874c6b1ae
Sulav Shakya <sulav_shakya@hotmail.com>
2018-09-23 22:18:01 -0500

    SulavMultiply

---
+++ Multiplication/Multiplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication
+{
+    public class Multiplier
+    {
+        public float AddSubDivMul(float anurag, float b)
+        {
+            return anurag * b;
+        }
+    }
+}
